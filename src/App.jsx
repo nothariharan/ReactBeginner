f28b1f336f7dad82a2d4fd2087e7100928ee150e
nothariharan <nothariharan@gmail.com>
2025-09-26 10:13:35 +0530

    made calculator with eval function lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Meals from './components/Meals'
+import Calculator from './components/Calculator'
+
 
 
 
 const App = () => {
   return (
     <div>
-      <Meals />
+      <Calculator />
     </div>
   )
 }
